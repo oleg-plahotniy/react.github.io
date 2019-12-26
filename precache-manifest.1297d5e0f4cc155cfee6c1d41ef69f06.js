d2bb1dbafc3b3897ab892d6c0791e4155d95af0b
Oleg Plahotniy <oleg-cvc@rambler.ru>
2019-12-26 22:29:21 +0200

    Updates

---
+++ precache-manifest.1297d5e0f4cc155cfee6c1d41ef69f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b0507ec8f64428eec2f5ad1fa7a4dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "952cba1e9112b0eb4e71",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1d941b2546df57d436ba",
+    "url": "/static/js/2.f8cb1079.chunk.js"
+  },
+  {
+    "revision": "952cba1e9112b0eb4e71",
+    "url": "/static/js/main.e520f958.chunk.js"
+  },
+  {
+    "revision": "655920dbce842ce573e0",
+    "url": "/static/js/runtime-main.58ec850a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
